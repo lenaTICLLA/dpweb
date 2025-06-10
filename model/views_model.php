86a9fc4a41e4bb8c090f4bce00ad131bde37b5f6
lenaTICLLA <lticllamedina@gmail.com>
2025-06-10 09:31:26 -0500

    practica3

--- model/views_model.php
+++ model/views_model.php
@@ -1,10 +1,10 @@
 <?php
 class viewModel{
     protected static function get_view($view){
-        $white_list = ["home", "products", "users"];
+        $white_list = ["home", "products", "users","new-user"];
         if (in_array($view, $white_list)) {
-            if (is_file("./view/".$view."php")) {
-                $content = "./view/".$view."php";
+            if (is_file("./view/".$view.".php")) {
+                $content = "./view/".$view.".php";
             }else{
                 $content = "404";
             }
