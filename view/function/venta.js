b259c042e2b9ea580ef6823c91b4b01e77e06d44
lenaTICLLA <lticllamedina@gmail.com>
2025-11-10 12:51:27 -0500

    practica34

---
+++ view/function/venta.js
@@ -0,0 +1,19 @@
+let prodructos_venta = [];
+let id = 2;
+let id2 = 4;
+let producto = {};
+    producto.nombre = "Producto A";
+    producto.precio = 100;
+    producto.cantidad = 2;
+
+let producto2 = {};
+    producto2.nombre = "Producto B";
+    producto2.precio = 200;
+    producto2.cantidad = 1;
+    //prodructos_venta.push(producto);
+
+
+prodructos_venta.id=producto;
+prodructos_venta[id2]=producto2;
+console.log(prodructos_venta);
+
